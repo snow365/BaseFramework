67cdc6ed10a43b3793e0fb0c0ad0e4bd069fa5f8
dfw <dfw@dfw.com>
2019-03-18 11:00:42 +0800

    增加mediainfo模块，并写获取长宽测试方法

---
+++ framework/src/main/java/com/dfw/framework/media/MediaInfoUtil.java
@@ -0,0 +1,23 @@
+package com.dfw.framework.media;
+
+import java.io.File;
+
+import com.abercap.mediainfo.api.MediaInfo;
+
+public class MediaInfoUtil {
+	public void getMediaInfo()
+	{
+		MediaInfo mediaInfo = new MediaInfo();
+		mediaInfo.open(new File("F:\\BaiduYunDownload\\深海越狱.Black.Water.20182018.中文字幕.BDrip.AAC.720P.x264-Lee.mp4"));
+		String type = mediaInfo.get(MediaInfo.StreamKind.General, 0, "FileExtension", MediaInfo.InfoKind.Text);
+        String width = mediaInfo.get(MediaInfo.StreamKind.Video, 0, "Width", MediaInfo.InfoKind.Text);
+        String height = mediaInfo.get(MediaInfo.StreamKind.Video, 0, "Height", MediaInfo.InfoKind.Text);
+        System.out.println(type);
+        System.out.println(width);
+        System.out.println(height);
+	}
+	public static void main(String[] args) {
+		MediaInfoUtil mu = new MediaInfoUtil();
+		mu.getMediaInfo();
+	}
+}
