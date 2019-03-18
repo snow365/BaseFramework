970a3758ce619b096a3250b1d1b3eb09e24afee8
dfw <dfw@dfw.com>
2019-03-18 11:16:02 +0800

    判断事物是否开启。

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BaseFramework
 Spring 5
 mybatis 3.4.6
+###Componments
+1，mediainfo 18.2.0.0 ，mediainfoapi
--- framework/src/main/java/com/dfw/framework/index/IndexServiceImpl.java
+++ framework/src/main/java/com/dfw/framework/index/IndexServiceImpl.java
@@ -2,6 +2,7 @@ package com.dfw.framework.index;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import com.dfw.framework.orm.BaseDao;
 
@@ -11,6 +12,10 @@ public class IndexServiceImpl implements IndexService{
 	@Autowired
 	BaseDao baseDao;
 	public String test2() {
+		if(!TransactionSynchronizationManager.isSynchronizationActive())
+		{
+			System.err.println("事务未开启。");
+		}
 		baseDao.test2();
 		return null;
 	}
