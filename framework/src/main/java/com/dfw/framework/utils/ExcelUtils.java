5cd01df4b1cafb726cc2c5c2d0449e289b487c11
snow365 <nsf001@163.com>
2019-05-31 17:18:31 +0800

    增加excelutils

---
+++ framework/src/main/java/com/dfw/framework/utils/ExcelUtils.java
@@ -0,0 +1,23 @@
+package com.dfw.framework.utils;
+
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
+public class ExcelUtils {
+
+	/***
+	 * 使用SXSSFWorkbook写入大量数据的excel。
+	 * @return 返回生成的excel路径
+	 */
+	public static String WriteExcel()
+	{
+		SXSSFWorkbook sb = new SXSSFWorkbook(1000); //flush缓冲区数量
+		
+		return null;
+	}
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
