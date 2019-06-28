a508654d5b866edb3c48cb27c677e00aa70fd43c
zhaoxt <zhaoxt@hangyjx.com>
2019-06-28 17:15:54 +0800

    增加方法

--- framework/src/main/java/com/dfw/framework/utils/CommonUtils.java
+++ framework/src/main/java/com/dfw/framework/utils/CommonUtils.java
@@ -1,7 +1,11 @@
 package com.dfw.framework.utils;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+
+import com.alibaba.fastjson.JSON;
 
 public class CommonUtils {
 	/**
@@ -19,6 +23,36 @@ public class CommonUtils {
 	public static String getDateByFmt(String df)
 	{
 		return new SimpleDateFormat(df).format(new Date());
+	}
+	/***
+	 * 将数字年份转换成中文
+	 * @param number 如 2019
+	 * @return 二〇一九
+	 */
+	@SuppressWarnings("unchecked")
+	public static String NumberYear2CHS(String number)
+	{
+		Map<String,String> map = (Map<String, String>) JSON.parse(
+				"{\"0\":\"〇\","
+				+ "\"1\":\"一\","
+				+ "\"2\":\"二\","
+				+ "\"3\":\"三\","
+				+ "\"4\":\"四\","
+				+ "\"5\":\"五\","
+				+ "\"6\":\"六\","
+				+ "\"7\":\"七\","
+				+ "\"8\":\"八\","
+				+ "\"9\":\"九\"}"
+				);
+		String chs = "";
+		for(int i=0;i<number.length();i++)
+		{
+			chs +=map.get(String.valueOf(number.charAt(i)));
+		}
+		return chs;
+	}
+	public static void main(String[] args) {
+		System.out.println(NumberYear2CHS("2019"));
 	}
 	
 }
